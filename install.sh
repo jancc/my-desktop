8a5f4297a27e689a3778807c066c10deaa93262b
Jan Wolff <jan1996@hotmail.de>
2016-12-28 18:46:54 +0100

    wallpaper under fucking source control! :D

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 mkdir -p ~/.janstuff
 cp click.wav ~/.janstuff/click.wav
+cp wallpaper.jpg ~/.janstuff/wallpaper.jpg
 cp i3-config ~/.config/i3/config
 cp redshift.conf ~/.config/redshift.conf
 sudo install pulseaudio-set-all-sink-inputs.sh /usr/local/bin/pulseaudio-set-all-sink-inputs
