718785d58d0ca4e030abbb0e8267697a75fd954a
Jan Wolff <jan1996@hotmail.de>
2016-12-29 00:18:26 +0100

    i3 status with my battery lol

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 mkdir -p ~/.janstuff
 cp click.wav ~/.janstuff/click.wav
 cp wallpaper.jpg ~/.janstuff/wallpaper.jpg
+mkdir -p ~/.config/i3
 cp i3-config ~/.config/i3/config
+mkdir -p ~/.config/i3status
+cp i3status-config ~/.config/i3status/config
 cp redshift.conf ~/.config/redshift.conf
 sudo install pulseaudio-set-all-sink-inputs.sh /usr/local/bin/pulseaudio-set-all-sink-inputs
 
