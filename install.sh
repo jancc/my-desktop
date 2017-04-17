b86b527c0e7b44c2c503c2dadc1e315cd7f19159
Jan Wolff <jan1996@hotmail.de>
2017-04-17 11:22:30 +0200

    Added irssi config, x11 scripts, new i3 stuff

--- install.sh
+++ install.sh
@@ -7,5 +7,8 @@ cp i3-config ~/.config/i3/config
 mkdir -p ~/.config/i3status
 cp i3status-config ~/.config/i3status/config
 cp redshift.conf ~/.config/redshift.conf
+cp irssi-config ~/.irssi/config
 sudo install pulseaudio-set-all-sink-inputs.sh /usr/local/bin/pulseaudio-set-all-sink-inputs
 sudo install brightness.sh /usr/local/bin/brightness-set
+sudo install x11-enable-screensaving.sh /usr/local/bin/x11-enable-screensaving
+sudo install x11-disable-screensaving.sh /usr/local/bin/x11-disable-screensaving
