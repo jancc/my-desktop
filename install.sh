5eb58a1d1b77e882a751efaa1eb0670e8e6a2bc8
Jan Wolff <jan1996@hotmail.de>
2017-05-08 22:28:56 +0200

    Added zshrc

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ cp i3status-config ~/.config/i3status/config
 cp redshift.conf ~/.config/redshift.conf
 cp irssi-config ~/.irssi/config
 cp Xdefaults ~/.Xdefaults
+cp zshrc ~/.zshrc
 sudo install pulseaudio-set-all-sink-inputs.sh /usr/local/bin/pulseaudio-set-all-sink-inputs
 sudo install brightness.sh /usr/local/bin/brightness-set
 sudo install x11-enable-screensaving.sh /usr/local/bin/x11-enable-screensaving
