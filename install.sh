3081d9965aa651206e85ed3ef06235fa7c1117ca
Jan Wolff <jan1996@hotmail.de>
2017-10-31 19:15:46 +0100

    use symlinks, fix indenting in vim

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #/bin/sh
 mkdir -p ~/.janstuff
-cp click.wav ~/.janstuff/click.wav
-cp wallpaper.jpg ~/.janstuff/wallpaper.jpg
+ln -f -s $PWD/click.wav ~/.janstuff/click.wav
+ln -f -s $PWD/wallpaper.jpg ~/.janstuff/wallpaper.jpg
 mkdir -p ~/.config/i3
-cp i3-config ~/.config/i3/config
+ln -f -s $PWD/i3-config ~/.config/i3/config
 mkdir -p ~/.config/i3status
-cp i3status-config ~/.config/i3status/config
-cp redshift.conf ~/.config/redshift.conf
-cp irssi-config ~/.irssi/config
-cp Xdefaults ~/.Xdefaults
-cp zshrc ~/.zshrc
-cp vimrc ~/.vimrc
+ln -f -s $PWD/i3status-config ~/.config/i3status/config
+ln -f -s $PWD/redshift.conf ~/.config/redshift.conf
+ln -f -s $PWD/irssi-config ~/.irssi/config
+ln -f -s $PWD/Xdefaults ~/.Xdefaults
+ln -f -s $PWD/zshrc ~/.zshrc
+ln -f -s $PWD/vimrc ~/.vimrc
 sudo install pulseaudio-set-all-sink-inputs.sh /usr/local/bin/pulseaudio-set-all-sink-inputs
 sudo install brightness.sh /usr/local/bin/brightness-set
 sudo install x11-enable-screensaving.sh /usr/local/bin/x11-enable-screensaving
