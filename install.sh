80eb115faf81e9f566a0b0a2694db98d1e58c563
Jan Wolff <janw@mailbox.org>
2018-12-23 11:11:27 +0100

    remove some stuff i dont use daily from install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 ln -f -s $PWD/irssi-config ~/.irssi/config
-ln -f -s $PWD/Xdefaults ~/.Xdefaults
-ln -f -s $PWD/zshrc ~/.zshrc
 ln -f -s $PWD/vimrc ~/.vimrc
 ln -f -s $PWD/tmux.conf ~/.tmux.conf
 mkdir -p ~/.config/mpd
 ln -f -s $PWD/mpd.conf ~/.config/mpd/mpd.conf
 ln -f -s $PWD/fish ~/.config/fish
-ln -f -s $PWD/urxvt ~/.urxvt
-sudo install pulseaudio-set-all-sink-inputs.sh /usr/local/bin/pulseaudio-set-all-sink-inputs
-sudo install brightness.sh /usr/local/bin/brightness-set
 sudo install ssh-tunnel.sh /usr/local/bin/ssh-tunnel
