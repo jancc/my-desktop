2fbaeee0f8ec2aed6d17f7835a5fa1f713d431d3
Jan Wolff <janw@mailbox.org>
2018-12-30 18:27:25 +0100

    jwm :D

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ ln -f -s $PWD/tmux.conf ~/.tmux.conf
 mkdir -p ~/.config/mpd
 ln -f -s $PWD/mpd.conf ~/.config/mpd/mpd.conf
 ln -f -s $PWD/fish ~/.config/fish
+ln -f -s $PWD/urxvt ~/.urxvt
+ln -f -s $PWD/Xdefaults ~/.Xdefaults
 sudo install ssh-tunnel.sh /usr/local/bin/ssh-tunnel
