3d118fee24549ca5654516aead553e391639b0c8
Jan Wolff <janw@mailbox.org>
2019-01-13 13:17:06 +0100

    volume in i3-blocks, ssh agent in fish, scrot keybind

--- fish/config.fish
+++ fish/config.fish
@@ -8,8 +8,18 @@ function did
     vim +'norm Go' +'r!date' +'norm o' ~/Sync/did.txt
 end
 
+function init_ssh_agent
+    if not pgrep -u "$USER" ssh-agent > /dev/null
+        ssh-agent -c > ~/.ssh-agent-values
+    end
+    if test -z "$SSH_AGENT_PID"
+        source ~/.ssh-agent-values
+    end
+end
+
 set -U fish_greeting
 set -U EDITOR vim
 set -U GOPATH $HOME/code/go
 
 # init_gui
+init_ssh_agent
