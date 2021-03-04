656f5911152b44969d1242226de7692ec15b0270
Jan Wolff <janw@mailbox.org>
2021-03-04 10:30:26 +0100

    aerc

--- stowthis/qutebrowser/.config/qutebrowser/config.py
+++ stowthis/qutebrowser/.config/qutebrowser/config.py
@@ -1,3 +1,4 @@
+config.load_autoconfig()
 c.url.default_page = "https://ecosia.org"
 c.url.start_pages = ["https://ecosia.org"]
 c.url.searchengines = {"DEFAULT": "https://ecosia.org/search?q={}"}
@@ -5,6 +6,5 @@ c.content.javascript.enabled = False
 
 with config.pattern("*://ecosia.org/") as p:
     p.content.javascript.enabled = True
-    p.content.host_blocking.enabled = False
 
 config.bind('m', 'spawn mpv {url}')
